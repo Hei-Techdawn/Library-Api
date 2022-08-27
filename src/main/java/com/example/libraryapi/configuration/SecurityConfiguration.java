6fb08f9aa3c9eb38f388bb85663ad36deaeb92e9
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-27 09:49:20 +0300

    fix : disable security

--- src/main/java/com/example/libraryapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/libraryapi/configuration/SecurityConfiguration.java
@@ -1,50 +1,50 @@
-package com.example.libraryapi.configuration;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-@AllArgsConstructor
-@Slf4j
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private UserDetailsService userDetailsService;
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder());
-        log.warn("PASSWORD admin :  " + new BCryptPasswordEncoder().encode("admin"));
-        return provider;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/").permitAll()
-                .antMatchers(HttpMethod.GET,"/book").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .logout().permitAll()
-                .and()
-                .cors()
-                .and()
-                .csrf().disable()
-                .httpBasic();
-    }
-}
+//package com.example.libraryapi.configuration;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//@AllArgsConstructor
+//@Slf4j
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    private UserDetailsService userDetailsService;
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(userDetailsService);
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//        log.warn("PASSWORD admin :  " + new BCryptPasswordEncoder().encode("admin"));
+//        return provider;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/").permitAll()
+//                .antMatchers(HttpMethod.GET,"/book").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .logout().permitAll()
+//                .and()
+//                .cors()
+//                .and()
+//                .csrf().disable()
+//                .httpBasic();
+//    }
+//}
